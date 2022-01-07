568f6a8edcaf7123ac0cc6aaa2d2990298fdd433
Aden Potts <adenpotts01@icloud.com>
2022-01-07 03:31:30 -0500

    Update api-interface.js

--- exports/api-interface.js
+++ exports/api-interface.js
@@ -14,28 +14,32 @@ module.exports = {
             }
         }
 
-        var req = https.get(options, (res) => {
-            console.log("[API] Got response code: " + res.statusCode);
-
-            var jsondata = '';
-                
-            res.on('data', (chunk) => {
-                jsondata += chunk;
+        try {
+            var req = https.get(options, (res) => {
+                console.log("[API] Got response code: " + res.statusCode);
+    
+                var jsondata = '';
+                    
+                res.on('data', (chunk) => {
+                    jsondata += chunk;
+                });
+    
+                res.on('end', () => {
+                    console.log(jsondata);
+                    try {
+                        jsondata = JSON.parse(jsondata);
+                    } catch (e) {
+                        console.log(`[API] Error parsing data! ${e}\nResult: ${jsondata}`);
+                    }
+    
+                    if(cb) {
+                        cb(jsondata);
+                    }
+                });
             });
-
-            res.on('end', () => {
-                console.log(jsondata);
-                try {
-                    jsondata = JSON.parse(jsondata);
-                } catch (e) {
-                    console.log(`[API] Error parsing data! ${e}\nResult: ${jsondata}`);
-                }
-
-                if(cb) {
-                    cb(jsondata);
-                }
-            });
-	    });
+        } catch (e) {
+            console.log(e);
+        }
     },
     POST: (endpoint, data, cb = null) => {
 
