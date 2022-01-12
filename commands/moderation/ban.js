67d7b516c023261d11805e1444f34a5d62835a8b
Aden Potts <adenpotts01@icloud.com>
2022-01-12 09:27:53 -0500

    Added !ban, fixed !kick

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,11 +1,12 @@
 const Discord = require("discord.js");
+const Logger = require("../../exports/logging");
 
 module.exports = {
-    name: 'kick',
-    description: 'Kick a user.',
+    name: 'ban',
+    description: 'Bans a user.',
     usage: '<@user> <reason>',
     category: 'Moderation',
-    permission: 'KICK_MEMBERS',
+    permission: 'BAN_MEMBERS',
     reqargs: 2,
     execute(message, args){
         if(!message.mentions.users.size) return message.reply('You need to tag a user!');
@@ -14,7 +15,7 @@ module.exports = {
         const taggedMember = message.mentions.members.first();
 
         if(taggedMember.hasPermission('ADMINISTRATOR')){
-            message.reply("You cannot kick senior staff!");
+            message.reply("You cannot ban senior staff!");
             return;
         }
         let executeUser = message.member.user;
@@ -24,6 +25,7 @@ module.exports = {
 
         const msg = message.client.EmbedMessage("Message", {name: 'Success!', value: `Kicked ${taggedUser.username} for **${reason}**`}, executeUser, '');
         message.reply({embeds: [msg]});
-        taggedUser.send("Ah shucks! You've been discarded from the FiveM Discord. That sucks!\nReason for *kick*: ```" + reason + "```");
+        taggedUser.send("Ah shucks! You've been discarded from the FiveM Discord. That sucks!\nReason for *ban*: ```" + reason + "```");
+        taggedMember.ban({days: 1, reason: reason}).catch(Logger.Error);
     }
 }
