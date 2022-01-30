2919e2fd4364e1d01d98ff64c8aa4b4bd79f811f
Aden Potts <adenpotts01@icloud.com>
2022-01-29 22:42:36 -0500

    rewrote duty

--- commands/police/duty.js
+++ commands/police/duty.js
@@ -1,10 +1,12 @@
 const Discord = require("discord.js");
+const API = require("../../exports/api-interface");
+const Prospect = require("../../exports/prospect-utils");
 
 module.exports = {
     name: 'duty',
     description: 'Get your duty time.',
     usage: '<NONE>',
-    category: 'Police',
+    category: 'Utilities',
     policeperms: 'all',
     permission: 0,
     reqargs: 0,
@@ -12,72 +14,64 @@ module.exports = {
 
 		var client = message.client;
 
-    	client.fivemQuery('SELECT * FROM `users` WHERE `discord` = \'discord:' + message.member.id + '\'', function(success, res){
-        	if(!success){
-            	message.channel.send(`Uh oh! An error occured!`);
-            	return;
-        	}else if(!res[0]){
-           		message.channel.send("Uh oh! No database results were found for this user.");
-            	return;
-        	}
-			let identifier = res[0].identifier;
+        API.GET(`discord/getdutytime/${message.member.id}`, (data) => {
+            if(data.Code != 200) {
+                Prospect.TimedReply(message, `An error appeared. Message a developer this...\`\`\`${data}\`\`\``);
 
-			client.fivemQuery("SELECT * FROM `characters` WHERE `identifier` = '" + identifier + "' AND `job` = 1", (success, res) => {
-				if(!success){
-					message.channel.send(`Uh oh! An error occured!`);
-					return;
-				}else if(!res[0]){
-					message.channel.send("Uh oh! No database results were found for this user.");
-					return;
-				}
+                return;
+            } else if(data.Response.length == 0) {
+                Prospect.TimedReply(message, "I didn't find any data for you. Try again later, or place a support ticket in.");
 
-				let total = 0;
-				let cname = '';
-				let mostActiveChar = "";
-				let mostActiveCharTime = 0;
-				let fields = [];
-				let time = 0;
+                return;
+            }
 
+            let res = data['Response'];
 
-				for(i = 0; i < res.length; i++){
-					var c = res[i];
-					
-					cname = c.firstname + " " + c.lastname;
-					time = parseFloat((c.dutyPlaytime / 3600)).toFixed(1);
+            let total = 0;
+            let cname = '';
+            let mostActiveChar = "";
+            let mostActiveCharTime = 0;
+            let fields = [];
+            let time = 0;
+
+            for(i = 0; i < res.length; i++){
+                var c = res[i];
+                
+                cname = c.firstname + " " + c.lastname;
+                time = parseFloat((c.dutyPlaytime / 3600)).toFixed(1);
 
 
-					if(mostActiveCharTime == 0){
-						mostActiveChar = c.firstname + " " + c.lastname;
-						mostActiveCharTime = parseFloat((res[i].dutyPlaytime/3600)).toFixed(1);
-					}
+                if(mostActiveCharTime == 0){
+                    mostActiveChar = c.firstname + " " + c.lastname;
+                    mostActiveCharTime = parseFloat((res[i].dutyPlaytime/3600)).toFixed(1);
+                }
 
-					if(!mostActiveCharTime == 0 && time > mostActiveCharTime){
-						mostActiveChar = c.firstname + " " + c.lastname;
-						mostActiveCharTime = parseFloat((res[i].dutyPlaytime/3600)).toFixed(1);
-					}
+                if(!mostActiveCharTime == 0 && time > mostActiveCharTime){
+                    mostActiveChar = c.firstname + " " + c.lastname;
+                    mostActiveCharTime = parseFloat((res[i].dutyPlaytime/3600)).toFixed(1);
+                }
 
-					total += time;
+                total += time;
 
-					fields.push({name: cname, value: `\`\`${time}\`\` hours`, inline: true});
-				}
-				total = parseFloat(total).toFixed(1);
+                fields.push({name: cname, value: `\`\`${time}\`\` hours`, inline: true});
+            }
+            total = parseFloat(total).toFixed(1);
 
-				fields.push({name: "Most Active Character", value: `${mostActiveChar} with \`\`${mostActiveCharTime}\`\` hours`});
+            fields.push({name: "Most Active Character", value: `${mostActiveChar} with \`\`${mostActiveCharTime}\`\` hours`});
 
-				const msg = new Discord.MessageEmbed()
-				.setColor('#0099ff')
-				.setTitle("Time on Duty")
-				.setAuthor(client.user.username, client.user.avatarURL())
-				.setDescription(`${message.member.user.username}'s duty time\n${total} hours clocked on duty.`)
-				.setThumbnail(message.member.user.avatarURL())
-				.addFields(
-					fields,
-				)
-				.setTimestamp()
-				.setFooter('Executed by ' + message.member.user.username, message.member.user.avatarURL());
+            const msg = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle("Time on Duty")
+            .setAuthor(client.user.username, client.user.avatarURL())
+            .setDescription(`${message.member.user.username}'s duty time\n${total} hours clocked on duty.`)
+            .setThumbnail(message.member.user.avatarURL())
+            .addFields(
+                fields,
+            )
+            .setTimestamp()
+            .setFooter('Executed by ' + message.member.user.username, message.member.user.avatarURL());
 
-				message.channel.send({embeds: [msg]});
-			});
-    	});
+            message.reply({embeds: [msg]}).catch(Logger.Error);
+        });
     }
 }
